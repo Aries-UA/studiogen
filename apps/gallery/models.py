b59719bf493aff6cd0a95f65fed80582e94cfdec
aries <aries@aries-vrt>
2012-12-06 17:59:40 +0200

    Фикс определения файла на диске

--- apps/gallery/models.py
+++ apps/gallery/models.py
@@ -57,7 +57,7 @@ class Gallery(models.Model):
     def original(self):
         name = get_image_name(self.img.name)
         name = (self.img.name).replace(name, '_'.join([name, '740x494']))
-        if not os.path.isfile(name):
+        if not os.path.isfile(''.join([MEDIA_ROOT, name])):
             name = get_image_name(self.img.name)
             name = (self.img.name).replace(name, '_'.join([name, '600x400']))
         return '{0}{1}'.format(MEDIA_URL, name)
