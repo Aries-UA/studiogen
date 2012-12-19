02a3158108f4b172b43e1b0e0c7fc77d6a83a9f4
aries <aries@aries-vrt>
2012-12-19 11:13:53 +0200

    Правка статики

--- apps/news/views.py
+++ apps/news/views.py
@@ -6,10 +6,12 @@ from django.http import HttpResponse, HttpResponseRedirect, HttpRequest
 from django.shortcuts import render
 from django.core.urlresolvers import reverse
 from apps.news.models import News
+from django.utils import translation
 
 def news(request):
     data = {
         'news': News.objects.all(),
+        'LANGUAGE_CODE': translation.get_language(),
     }
     return render(request, 'news_news.html', data)
 
@@ -18,6 +20,7 @@ def one_news(request, news_id):
     if News.objects.filter(id=news_id).exists():
         data = {
             'one': News.objects.filter(id=news_id)[0],
+            'LANGUAGE_CODE': translation.get_language(),
         }
         return render(request, 'news_one.html', data)
     else:
